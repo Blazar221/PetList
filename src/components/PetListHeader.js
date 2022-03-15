3b01df79704ae6e95bdea8e5e0d51537fe56bb37
Blazar <805781038@qq.com>
2022-03-14 19:57:27 -0700

    add Header

---
+++ src/components/PetListHeader.js
@@ -0,0 +1,14 @@
+import React from "react"
+import styled from "styled-components";
+
+class PetListHeader extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return <h1>This is a header</h1>
+    }
+}
+
+export default PetListHeader;
