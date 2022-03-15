a25aec4418c6dc35a6ada922c1d75e8b2f6ee559
Blazar <805781038@qq.com>
2022-03-15 16:38:05 -0700

    add card shadow transition and improve by shouldUpate

--- src/components/PetCard.js
+++ src/components/PetCard.js
@@ -2,6 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
+  &:hover {
+    box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.63);
+  }
+  
   img {
     width: 18vw;
     aspect-ratio: 1;
@@ -34,11 +38,12 @@ const Wrapper = styled.div`
   aspect-ratio: 0.63;
 
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5);
+  transition: 0.2s;
   border-radius: 20px;
 
   margin: 1vw;
 
-  background: ${props => props.type === 1 ? "#CCFF66" : props.type === 2 ? "#66CCCC" : "#FFCC00"}
+  background: ${props => props.type === 1 ? "#CCFF66" : props.type === 2 ? "#66CCCC" : "#FFCC00"};
 `
 
 
@@ -83,7 +88,7 @@ class PetCard extends React.Component {
         //console.log("renderCard");
         //console.log(this.props.isChecked)
         return (
-            <Wrapper type={this.props.type} willShow={this.props.willShow}>
+            <Wrapper type={this.props.type} willShow={this.props.willShow} onClick={this.checkChange}>
                 <input type="checkbox" onChange={this.checkChange} checked={this.props.isChecked}/>
                 <img src={this.props.imageUrl} alt={this.props.title}/>
                 <Content type={this.props.type}>
